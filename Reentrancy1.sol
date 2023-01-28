003ef8f3644c64b763b40119e8452a474a008c00
BCHSBC <skhs0199@gmail.com>
2023-01-28 21:44:17 +0900

    Using openzepplin

--- Reentrancy1.sol
+++ Reentrancy1.sol
@@ -1,19 +1,18 @@
 //SPDX-License-Identifier: GPL-30
 pragma solidity >= 0.8.1;
-
-contract Bank{
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/ReentrancyGuard.sol";
+contract Bank is ReentrancyGuard{
     mapping(address=> uint) public balances;
 
     function deposit() external payable {
         balances[msg.sender] += msg.value;
     }
 
-    function withdraw() external {
+    function withdraw() external nonReentrant(){
         uint currentBalance = balances[msg.sender];
-        balances[msg.sender] = 0;
         (bool result,) = msg.sender.call{value:currentBalance}("");
         require(result,"ERROR");
-        // balances[msg.sender] = 0;
+        balances[msg.sender] = 0;
     }
 
     function checkBalance() external view returns(uint) {
